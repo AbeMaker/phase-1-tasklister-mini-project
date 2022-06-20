69adf43930824fabb503fc71210cd1dca049b3c1
Abraham Anyang <anyangmaker90@gmail.com>
2022-06-20 17:39:52 +0300

    Assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTask = document.getElementById("create-task-form");
+  const description = document.getElementById("new-task-description");
+  const ul = document.getElementById("tasks");
+  newTask.addEventListener("submit", creatNewTask)
+
+  document.querySelector("form").addEventListener("submit",(e)=>{
+    e.preventDefault()
+  })
 });
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+      event.preventDefault()
+      handleToDo(event.target.newtask.value)
+      form.reset()
+  })
+
+})
